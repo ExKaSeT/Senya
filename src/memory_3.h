e44b7f69e8d4fb3fa65261f39ce80ff0415cba7c
ExKaSeT <exkaset@mail.ru>
2023-06-16 13:44:14 +0300

    .

--- src/memory_3.h
+++ src/memory_3.h
@@ -17,7 +17,10 @@ public:
 	};
 
 private:
-	// method | alloc_ptr | logger_ptr | first_available_ptr
+	// available block: size_t | next_ptr
+	// occupied block: size_t
+	
+	// data: method | alloc_ptr | logger_ptr | first_available_ptr | ...
 	char* data;
 
 	bool has_logger() const
@@ -87,7 +90,7 @@ public:
 
 	memory_3(size_t data_size, allocation_method method, memory* alloc = nullptr, logger* log = nullptr)
 	{
-		if (data_size < get_service_block_size() + sizeof(size_t) + sizeof(void*))
+		if (data_size < get_service_block_size() + get_available_block_service_size())
 			throw std::bad_alloc();
 
 		if (alloc == nullptr)
@@ -212,18 +215,18 @@ public:
 			is_requested_size_overridden = true;
 		}
 
-		void* update_next_block_to_previous;
+		void* update_next_block_in_previous;
 
 		if (is_requested_size_overridden)
 		{
-			update_next_block_to_previous = next_to_target_block;
+			update_next_block_in_previous = next_to_target_block;
 		}
 		else
 		{
-			update_next_block_to_previous = reinterpret_cast<void*>
+			update_next_block_in_previous = reinterpret_cast<void*>
 			(reinterpret_cast<char*>(target_block) + get_occupied_block_service_size() + requested_size);
 
-			auto* target_block_leftover_size = reinterpret_cast<size_t*>(update_next_block_to_previous);
+			auto* target_block_leftover_size = reinterpret_cast<size_t*>(update_next_block_in_previous);
 			*target_block_leftover_size =
 					get_block_size(target_block) - get_occupied_block_service_size() - requested_size;
 
@@ -232,9 +235,9 @@ public:
 		}
 
 		previous_to_target_block == nullptr
-		? *get_first_block_address_address() = update_next_block_to_previous
+		? *get_first_block_address_address() = update_next_block_in_previous
 		: *reinterpret_cast<void**>
-		(reinterpret_cast<char*>(previous_to_target_block) + sizeof(size_t)) = update_next_block_to_previous;
+		(reinterpret_cast<char*>(previous_to_target_block) + sizeof(size_t)) = update_next_block_in_previous;
 
 		auto* target_block_size_address = reinterpret_cast<size_t*>(target_block);
 		*target_block_size_address = requested_size;
@@ -276,7 +279,7 @@ public:
 
 		void* previous_block = nullptr;
 		void* current_block = get_first_block_address();
-		if (current_block == nullptr)
+		if (current_block == nullptr) // target is only one available
 		{
 			*get_first_block_address_address() = target_to_dealloc;
 
@@ -313,7 +316,7 @@ public:
 			*reinterpret_cast<void**>(block_to_deallocate_size + 1) = nullptr;
 		}
 		else if (reinterpret_cast<char*>(target_to_dealloc) + get_available_block_service_size() +
-				 get_block_size(target_to_dealloc) == current_block)
+				 get_block_size(target_to_dealloc) == current_block) // check and merge with right
 		{
 			void* next_block = get_available_next_block_address(current_block);
 			auto* target_size = reinterpret_cast<size_t*>(target_to_dealloc);
@@ -324,7 +327,7 @@ public:
 		}
 		if (previous_block != nullptr &&
 			reinterpret_cast<char*>(previous_block) + get_available_block_service_size() +
-			get_block_size(previous_block) == target_to_dealloc)
+			get_block_size(previous_block) == target_to_dealloc) // check and merge with left
 		{
 			void* next_block = get_available_next_block_address(target_to_dealloc);
 			auto* prev_block_size = reinterpret_cast<size_t*>(previous_block);
